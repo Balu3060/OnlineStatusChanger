ffb4242d5520fc443debc52b62d533d86c3365bb
Balu3060 <balazs.sipos118@gmail.com>
2026-03-05 00:10:30 +0100

    Add files via upload

---
+++ OnlineStatusChanger.h
@@ -0,0 +1,18 @@
+#pragma once
+#include "bakkesmod/plugin/bakkesmodplugin.h"
+#include "bakkesmod/plugin/pluginwindow.h"
+#include "bakkesmod/plugin/PluginSettingsWindow.h"
+
+class OnlineStatusChanger : public BakkesMod::Plugin::BakkesModPlugin, public BakkesMod::Plugin::PluginSettingsWindow
+{
+public:
+    virtual void onLoad() override;
+    virtual void onUnload() override;
+    
+    void RenderSettings() override;
+    std::string GetPluginName() override;
+    void SetImGuiContext(uintptr_t ctx) override;
+
+private:
+    void SetOnlineStatus(std::string eventName);
+};
