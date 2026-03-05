da4c0b87b826c43e3f807f1e0b6f9db4b266561f
Balu3060 <balazs.sipos118@gmail.com>
2026-03-05 02:24:42 +0100

    Update OnlineStatusChanger.h

--- OnlineStatusChanger.h
+++ OnlineStatusChanger.h
@@ -1,17 +1,11 @@
 #pragma once
 #include "bakkesmod/plugin/bakkesmodplugin.h"
-#include "bakkesmod/plugin/pluginwindow.h"
-#include "bakkesmod/plugin/PluginSettingsWindow.h"
 
-class StatusOverrider : public BakkesMod::Plugin::BakkesModPlugin, public BakkesMod::Plugin::PluginSettingsWindow
+class StatusOverrider : public BakkesMod::Plugin::BakkesModPlugin
 {
 public:
     virtual void onLoad() override;
     virtual void onUnload() override;
-    
-    void RenderSettings() override;
-    std::string GetPluginName() override;
-    void SetImGuiContext(uintptr_t ctx) override;
 
 private:
     void SetOnlineStatus(std::string eventName);
