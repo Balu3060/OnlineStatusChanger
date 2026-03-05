f02e2c3ec16694fc068e3bebed9e2bef39db3a3f
Balu3060 <balazs.sipos118@gmail.com>
2026-03-05 01:28:39 +0100

    Refactor OnlineStatusChanger with updated comments

--- OnlineStatusChanger.cpp
+++ OnlineStatusChanger.cpp
@@ -5,11 +5,9 @@ BAKKESMOD_PLUGIN(StatusOverrider, "Status Overrider", "1.0", 0)
 void StatusOverrider::onLoad()
 {
     cvarManager->registerCvar("cl_status_enable", "1", "Enable Status Overrider", true, true, 0, true, 1);
-    cvarManager->registerCvar("cl_status_override", "0", "Override Status (0-3)", true, true, 0, true, 3);
+    cvarManager->registerCvar("cl_status_override", "0", "Override Status", true, true, 0, true, 3);
 }
 
 void StatusOverrider::onUnload() {}
 
-void StatusOverrider::SetOnlineStatus(std::string eventName) {
-    // Logic remains same, but using renamed CVars
-}
+void StatusOverrider::SetOnlineStatus(std::string eventName) {}
